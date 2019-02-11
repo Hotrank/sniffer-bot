d65002f50b1d2359b1ff55545667ceb1357d8ec5
Hotrank <hmzhen@gmail.com>
2019-02-11 15:46:40 -0500

    added producer iterations

--- src/producer_from_local.py
+++ src/producer_from_local.py
@@ -4,6 +4,7 @@ import os
 
 
 SOURCE_DIR = '../../test_images/'
+ITERATIONS = 50
 
 def producer():
     context = zmq.Context()
@@ -13,15 +14,16 @@ def producer():
     image_paths = [SOURCE_DIR + fname for fname in fnames]
 
     zmq_socket.send_json({'url': 'start'})
-    for i, image_path in numerate(image_paths):
-        with open(image_path,'rb') as f:
-            bytes = bytearray(f.read())
-            message = base64.b64encode(bytes)
-            meta_data = { 'url' : image_path }
-            zmq_socket.send_json(meta_data, flags=zmq.SNDMORE)
-            zmq_socket.send(message)
+    for iter in range(ITERATIONS):
+        for i, image_path in enumerate(image_paths):
+            with open(image_path,'rb') as f:
+                bytes = bytearray(f.read())
+                message = base64.b64encode(bytes)
+                meta_data = { 'url' : image_path }
+                zmq_socket.send_json(meta_data, flags=zmq.SNDMORE)
+                zmq_socket.send(message)
     zmq_socket.send_json({'url': 'done'})
-    print('All images sent. total:', len(fnames))
+    print('All images sent. total:', len(fnames)*ITERATIONS)
 
 if __name__ == '__main__':
     producer()
